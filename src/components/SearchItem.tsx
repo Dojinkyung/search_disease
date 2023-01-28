b3028101fe022fda84474fef25dab41b8ecc6587
Dojinkyung <63532503+Dojinkyung@users.noreply.github.com>
2023-01-28 21:00:37 +0900

    feat: 키보드로 버튼 이동 시 가운데로 보여주기+구글로 검색 링크

--- src/components/SearchItem.tsx
+++ src/components/SearchItem.tsx
@@ -1,25 +1,21 @@
 import { useSelector } from 'react-redux'
 import { SearchIcon } from '../assets/svgs'
 import { RootState } from '../redux/store'
-import { cx } from '../styles'
 import highlightedText from './Highlighted'
 import styles from './searchItem.module.scss'
 
 interface Props {
   diseaseName: string
-  isFocus: boolean
 }
 
-const SearchItem = ({ diseaseName, isFocus }: Props) => {
+const SearchItem = ({ diseaseName }: Props) => {
   const inputSearch = useSelector((state: RootState) => state.searchReducer.searchString)
 
   return (
-    <li className={cx(styles.item, { [styles.focusedItem]: isFocus })}>
-      <SearchIcon />
-      <button type='button' className={styles.items}>
-        <a href={`https://www.google.com/search?q=${diseaseName}`}>{highlightedText(diseaseName, inputSearch)}</a>
-      </button>
-    </li>
+    <a href={`https://www.google.com/search?q=${diseaseName}`} className={styles.items}>
+      <SearchIcon className={styles.icon} />
+      {highlightedText(diseaseName, inputSearch)}
+    </a>
   )
 }
 export default SearchItem
--- src/components/index.tsx
+++ src/components/index.tsx
@@ -6,20 +6,33 @@ import { setSearchString } from '../redux/slice'
 import { RootState } from '../redux/store'
 import useSearch from '../services/useSearch'
 import { Iitem } from '../types/search.d'
-import handleKeyboardFunc from '../services/useKeyboard'
 import LoadingOrNoSearch from './LoadingOrNoSearch'
 import styles from './index.module.scss'
 import SearchItem from './SearchItem'
+import { cx } from '../styles'
 
 const Search = () => {
   const dispatch = useDispatch()
   const inputSearch = useSelector((state: RootState) => state.searchReducer.searchString)
   const { items } = useSearch(inputSearch)
   const [index, setIndex] = useState<number>(-1)
-  const autoRef = useRef<HTMLUListElement>(null)
+  const autoRef = useRef<HTMLDivElement>(null)
 
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault()
+  useEffect(() => {
+    if (!autoRef.current) return
+
+    autoRef.current.scrollIntoView({
+      block: 'center',
+    })
+  }, [index])
+  const handleSelection = (selectedIndex: number) => {
+    if (items) {
+      const selectedItem = items[selectedIndex]
+
+      if (!selectedItem) return setIndex(-1)
+      window.location.assign(`https://www.google.com/search?q=${selectedItem.scientific_title_kr}`)
+    }
+    return items
   }
   const debouncedResults = useMemo(() => {
     const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
@@ -27,41 +40,63 @@ const Search = () => {
     }
     return debounce(handleChange, 800)
   }, [dispatch])
-
-  const handleListArrowKey = (e: React.KeyboardEvent<HTMLFormElement>) => {
-    handleKeyboardFunc(items, index, autoRef, setIndex, e)
-  }
   useEffect(() => {
     return () => {
       debouncedResults.cancel()
     }
   })
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLFormElement>) => {
+    const { key } = e
+    let nextIndexCount = 0
+    if (items) {
+      // move down
+      if (key === 'ArrowDown') nextIndexCount = (index + 1) % items.length
+
+      // move up
+      if (key === 'ArrowUp') nextIndexCount = (index + items.length - 1) % items.length
+
+      // hide search results
+      if (key === 'Escape') {
+        setIndex(-1)
+      }
+      // select the current item
+      if (key === 'Enter') {
+        e.preventDefault()
+        handleSelection(index)
+      }
+
+      setIndex(nextIndexCount)
+    }
+  }
 
   return (
     <main className={styles.centering}>
       <h1 className={styles.title}>국내 모든 임상시험 검색하고 온라인으로 참여하기</h1>
-      <form onSubmit={onSubmit} className={styles.form} role='presentation' onKeyDown={handleListArrowKey}>
-        <div className={styles.inputForm}>
-          <SearchIcon className={styles.searchIcon} />
-          <input
-            className={styles.input}
-            type='text'
-            placeholder='질환명을 입력해 주세요.'
-            onChange={debouncedResults}
-          />
-        </div>
-        <button className={styles.button} type='submit'>
-          검색
-        </button>
+      <form className={styles.inputForm} role='presentation' onKeyDown={handleKeyDown}>
+        <SearchIcon className={styles.searchIcon} />
+        <input
+          className={styles.input}
+          aria-label='Search'
+          type='search'
+          placeholder='질환명을 입력해 주세요.'
+          onChange={debouncedResults}
+        />
       </form>
+
       {inputSearch && (
-        <ul className={styles.ul} ref={autoRef}>
-          <li className={styles.search}>추천 검색어</li>
+        <div className={styles.ul}>
+          <div className={styles.search}>추천 검색어</div>
           <LoadingOrNoSearch />
           {items?.map((searched: Iitem, idx) => (
-            <SearchItem key={searched.trial_id} diseaseName={searched.scientific_title_kr} isFocus={idx === index} />
+            <div
+              key={searched.trial_id}
+              ref={idx === index ? autoRef : null}
+              className={cx(styles.item, { [styles.focusedItem]: idx === index })}
+            >
+              <SearchItem diseaseName={searched.scientific_title_kr} />
+            </div>
           ))}
-        </ul>
+        </div>
       )}
     </main>
   )
